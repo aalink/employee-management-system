62dbad3ce380aabef36a49c60aa145c59682194a
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-23 19:35:39 -0400

    Building out design of database schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,28 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE company_db;
+
+
+-- Makes department table
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+-- Makes role table
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
+
+-- Makes employee table
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+
+);
