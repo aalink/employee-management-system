7180b00163712aa4c77a93df716a811589caf38f
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-24 12:53:53 -0400

    Adds seed data for employee table

--- db/seed.sql
+++ db/seed.sql
@@ -30,3 +30,25 @@ VALUE
 
 
 -- Seed info for employee
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUE
+-- Engineering
+(1, 'Mark', 'Watney', 3, NULL),
+(2, 'Bruce', 'Ng', 2, 1),
+(3, 'Beth', 'Johanssen', 1, 1),
+-- Art & Design
+(4, 'Alex', 'Vogel', 6, NULL),
+(5, 'Mike', 'Watkins', 5, 4),
+(6, 'Tim', 'Grimes', 4, 4),
+-- Sales
+(7, 'Melissa', 'Lewis', 9, NULL),
+(8, 'Rick', 'Martinez', 8, 7),
+(9, 'Chris', 'Beck', 7, 7),
+-- Finance
+(10, 'Vincent', 'Kapoor', 12, NULL),
+(11, 'Mindy', 'Park', 11, 10),
+(12, 'Rich', 'Purnell', 10, 10),
+-- Legal
+(13, 'Teddy', 'Sanders', 15, NULL),
+(14, 'Annie', 'Montrose', 14, 13),
+(15, 'Mitch', 'Henderson', 13, 13);
