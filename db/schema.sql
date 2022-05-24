a4a5fa0a87eecfde250f8fed45d7d42da79f3716
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-23 23:29:15 -0400

    Establishing primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ USE employee_db;
 
 -- Makes department table
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 -- Makes role table
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL
@@ -20,7 +20,7 @@ CREATE TABLE role (
 
 -- Makes employee table
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT UNSIGNED PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
