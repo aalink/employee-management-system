bfb70dd8c3a292227f83cc946e5208e1158a3625
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-23 22:05:03 -0400

    Fixing USE database

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 -- Delete and then create a new database if it exists. Then use it.
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-USE company_db;
+USE employee_db;
 
 
 -- Makes department table
