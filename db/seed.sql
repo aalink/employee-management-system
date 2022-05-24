21df9885ae0203b17f50e714aac2cc4cb5e9d9f7
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-24 11:08:15 -0400

    Adds manager roles to the role table

--- db/seed.sql
+++ db/seed.sql
@@ -13,14 +13,19 @@ INSERT INTO role (title, salary, department_id)
 VALUE
 ('Software Engineer', 100000, 1),
 ('Hardware Engineer', 100000, 1),
+('Engineering Manager', 120000, 1),
 ('Graphic Designer', 50000, 2),
 ('Digital Sculptor', 60000, 2),
+('Art & Design Manager', 70000, 2),
 ('Junior Sales Representative', 50000, 3),
 ('Senior Sales Representative', 70000, 3),
-('Account Manager', 70000, 4),
+('Sales Manager', 80000, 3),
+('Actuary', 80000, 4),
 ('Financial Analyst', 80000, 4),
+('Finance Manager', 90000, 4),
 ('Patent Attorney', 90000, 5),
 ('Legal Analyst', 90000, 5);
+('Legal Manager', 100000, 5);
 
 
 
