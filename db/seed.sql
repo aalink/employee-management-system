c01db9ec1b122a54c2f45f45f53ef83599651b56
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-05-23 23:26:32 -0400

    Department seed data inserted

---
+++ db/seed.sql
@@ -0,0 +1,15 @@
+-- Seed info for department
+INSERT INTO department (name)
+VALUE 
+('Engineering'),
+('Design'),
+('Sales'), 
+('Finance'),
+('Legal');
+
+
+-- Seed info for role
+
+
+
+-- Seed info for employee
